e6299f42b39cb8fb6a38fed487d0ba07074d5e2e
Sweet <Sweet@Sweet>
2023-11-01 09:58:01 +0100

    make bots consider sudden death

--- src/sgame/sg_bot_ai.cpp
+++ src/sgame/sg_bot_ai.cpp
@@ -1378,7 +1378,7 @@ buildable_t BotChooseBuildableToBuild( gentity_t *self )
 		{
 			toBuild = BA_H_DRILL;
 		}
-		else if ( level.team[ G_Team( self ) ].numSpawns < g_bot_buildNumTelenodes.Get() )
+		else if ( !G_IsSuddenDeath() && level.team[ G_Team( self ) ].numSpawns < g_bot_buildNumTelenodes.Get() )
 		{
 			toBuild = BA_H_SPAWN;
 		}
@@ -1390,7 +1390,7 @@ buildable_t BotChooseBuildableToBuild( gentity_t *self )
 		{
 			toBuild = BA_H_MEDISTAT;
 		}
-		else
+		else if ( !G_IsSuddenDeath() )
 		{
 			toBuild = BA_H_MGTURRET;
 			if ( BG_BuildableUnlocked( BA_H_ROCKETPOD ) &&
@@ -1410,7 +1410,7 @@ buildable_t BotChooseBuildableToBuild( gentity_t *self )
 		{
 			toBuild = BA_A_LEECH;
 		}
-		else if ( level.team[ G_Team( self ) ].numSpawns < g_bot_buildNumEggs.Get() / 2 )
+		else if ( !G_IsSuddenDeath() && level.team[ G_Team( self ) ].numSpawns < g_bot_buildNumEggs.Get() / 2 )
 		{
 			toBuild = BA_A_SPAWN;
 		}
@@ -1418,11 +1418,11 @@ buildable_t BotChooseBuildableToBuild( gentity_t *self )
 		{
 			toBuild = BA_A_BOOSTER;
 		}
-		else if ( level.team[ G_Team( self ) ].numSpawns < g_bot_buildNumEggs.Get() )
+		else if ( !G_IsSuddenDeath() && level.team[ G_Team( self ) ].numSpawns < g_bot_buildNumEggs.Get() )
 		{
 			toBuild = BA_A_SPAWN;
 		}
-		else
+		else if ( !G_IsSuddenDeath() )
 		{
 			toBuild = BA_A_ACIDTUBE;
 		}
