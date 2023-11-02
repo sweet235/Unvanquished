acdf8eba5bbe543ded344b865e4c5855cdf471a8
Sweet <Sweet@Sweet>
2023-11-02 11:21:13 +0100

    at most one bot per frame may build

--- src/sgame/sg_struct.h
+++ src/sgame/sg_struct.h
@@ -677,6 +677,7 @@ struct level_locals_t
 		int              lastTeamStatus;
 		int              lastTacticId;
 		int              lastTacticTime;
+		int              lastBuildTime;
 	} team[ NUM_TEAMS ];
 
 	struct {
