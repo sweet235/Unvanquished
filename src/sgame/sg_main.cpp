814c7f216d745d9f757435788ba4424b2173d479
Sweet <Sweet@Sweet>
2025-01-03 15:35:16 +0100

    fix missing vampire notification after destroying a building under construction

--- src/sgame/sg_main.cpp
+++ src/sgame/sg_main.cpp
@@ -2419,9 +2419,6 @@ void G_RunFrame( int levelTime )
 	int        msec;
 	static int ptime3000 = 0;
 
-	extern void G_ResetStolenBP();
-	G_ResetStolenBP();
-
 	for ( team_t team : { TEAM_ALIENS, TEAM_HUMANS } )
 	{
 		level.team[ team ].unusedBP = unusedBP( team );
